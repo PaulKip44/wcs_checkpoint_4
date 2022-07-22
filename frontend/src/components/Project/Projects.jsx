3576d3ab1b0ab6bbdff9ff984f119fd92e322727
Paul SANCHEZ <paul.sanchez.44@gmail.com>
2022-07-22 12:31:08 +0200

    add auth gestion and projects list page

--- frontend/src/components/Project/Projects.jsx
+++ frontend/src/components/Project/Projects.jsx
@@ -1,5 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import ItemProject from "../ItemProject/ItemProject";
 import "./projects.css";
+// import CurrentUserContext from "../../contexts/currentUser";
 
-export default function Project() {
-  return <div className="projects">Ici la liste des Projects</div>;
+export default function Projects() {
+  const [listProjects, setListProjects] = useState([]);
+
+  // const { userProfil } = useContext(CurrentUserContext);
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/api/project`).then((res) => {
+      setListProjects(res.data);
+    });
+  }, []);
+
+  return (
+    <div className="languages">
+      <h1>My Projects</h1>
+      <ul>
+        {listProjects?.map((projectItem) => (
+          <li key={projectItem.Id}>
+            <ItemProject key={projectItem.name} project={projectItem} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
