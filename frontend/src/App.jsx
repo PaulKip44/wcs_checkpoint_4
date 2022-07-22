bd2c51acbdda4b52413acd0839c3a65a21aaf88f
Paul SANCHEZ <paul.sanchez.44@gmail.com>
2022-07-22 12:40:45 +0200

    fix again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,6 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 
-import "./app.css";
-
 function App() {
   return (
     <div className="App">
